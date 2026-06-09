5ac7513e678b471eddf66fd85ca61c4a10cfc6c7
nathanesky <esc21007@byui.edu>
2026-06-09 15:44:04 -0600

    u

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Express app is running!");
+  res.send("Express app is running! Yay");
 });
 
 app.listen(PORT, () => {
